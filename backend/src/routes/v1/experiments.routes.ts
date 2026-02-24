0581700fd8d5b035367e2e1f72cb56816081356d
K AKHIL KUMAR <140427573+Akhilkumar24@users.noreply.github.com>
2026-02-24 14:31:13 +0530

    Update experiments.routes.ts

--- backend/src/routes/v1/experiments.routes.ts
+++ backend/src/routes/v1/experiments.routes.ts
@@ -1,1 +1,19 @@
 
+import { Router } from "express";
+import {
+  getExperiment,
+  getExperiments,
+  postExperiment,
+  putExperiment,
+  removeExperiment,
+} from "../controllers/experiments.controller";
+
+const router = Router();
+
+router.get("/", getExperiments);
+router.get("/:id", getExperiment);
+router.post("/", postExperiment);
+router.put("/:id", putExperiment);
+router.delete("/:id", removeExperiment);
+
+export default router;
